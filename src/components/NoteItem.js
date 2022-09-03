28b7b01040234c80a04ad4e606ca0ac8f47ae10d
F_Riazi <f_riazi@isc.co.ir>
2022-09-03 11:40:15 +0430

    style

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,9 +4,9 @@ function NoteItem({items,focus}) {
   console.log(items,focus)
   return (
 
-    <div>
+    <div className='flex justify-center'>
         {items.length>0? items.map((item)=>(
-            <div style={{backgroundColor:item.bgColor}} key={item.id}>{item.note} </div>
+            <p className='m-4 p-2' style={{backgroundColor:item.bgColor}} key={item.id}>{item.note} </p>
         )):
         focus && <div> Note Not Found</div>
         }

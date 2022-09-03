b1207e44ce3d9e94ac1eeaf5e4b3862d24760e10
F_Riazi <f_riazi@isc.co.ir>
2022-09-03 11:29:05 +0430

    fix bug

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function NoteItem({items,focus}) {
+  console.log(items,focus)
   return (
+
     <div>
         {items.length>0? items.map((item)=>(
-            <div style={{backGroundColor:item.bgColor}} key={item.id}>{item.note} </div>
+            <div style={{backgroundColor:item.bgColor}} key={item.id}>{item.note} </div>
         )):
         focus && <div> Note Not Found</div>
         }
